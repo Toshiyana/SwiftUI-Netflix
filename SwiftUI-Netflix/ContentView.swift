fee320cb1be73de01960f02c5aa49418f413fbb6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-09 00:30:16 +0900

    Initial Commit

---
+++ SwiftUI-Netflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Netflix
+//
+//  Created by Toshiyana on 2022/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Netflix/SwiftUI_NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NetflixApp.swift
+//  SwiftUI-Netflix
+//
+//  Created by Toshiyana on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
