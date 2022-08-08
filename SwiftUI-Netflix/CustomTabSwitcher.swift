26d7f822645cac55c93ea36efcf1d8a86daabeb8
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-08-08 11:26:36 +0900

    add CustomTabSwitcher

---
+++ SwiftUI-Netflix/CustomTabSwitcher.swift
@@ -0,0 +1,76 @@
+//
+//  CustomTabSwitcher.swift
+//  SwiftUI-Netflix
+//
+//  Created by Toshiyana on 2022/08/08.
+//
+
+import SwiftUI
+
+struct CustomTabSwitcher: View {
+    @State private var currentTab: CustomTab = .episodes
+    
+    var tabs: [CustomTab]
+    
+    func widthForTab(_ tab: CustomTab) -> CGFloat {
+        let string = tab.rawValue
+        return string.widthOfString(usingFont: .systemFont(ofSize: 16, weight: .bold))
+    }
+    
+    var body: some View {
+        VStack {
+            // Custom Tab Picker
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(tabs, id: \.self) { tab in
+                        VStack {
+                            // Red Bar
+                            Rectangle()
+                                .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? .red : .clear)
+                            
+                            // Button
+                            Button(action: {
+                                currentTab = tab
+                            }, label: {
+                                Text(tab.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(tab == currentTab ? .white : .gray)
+                            })
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
+                        }
+                    }
+                }
+            }
+            
+            // Selected View
+            switch currentTab {
+            case .episodes:
+                Text("EPISODES")
+            case .trailers:
+                Text("TRAILERS")
+            case .more:
+                Text("MORE")
+            }
+        }//: VStack
+        .foregroundColor(.white)
+    }
+}
+
+enum CustomTab: String {
+    case episodes = "EPISODES"
+    case trailers = "TRAILERS & MORE"
+    case more = "MORE LIKE THIS"
+}
+
+struct CustomTabSwitcher_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            CustomTabSwitcher(tabs: [.episodes, .trailers, .more])
+        }
+    }
+}
