036cb0accb5137784441f1f0d438bfae5c248b5f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-08-08 23:58:17 +0900

    add EpisodesView

--- SwiftUI-Netflix/Helpers/GlobalHelpers.swift
+++ SwiftUI-Netflix/Helpers/GlobalHelpers.swift
@@ -24,6 +24,56 @@ let exampleTrailer3 = Trailer(name: "The Mysterious", videoURL: exampleVideoURL,
 
 let exampleTrailers = [exampleTrailer1, exampleTrailer2, exampleTrailer3]
 
+let episode1 = Episode(name: "Beginings and Endings",
+                       season: 1,
+                       episodeNumber: 1,
+                       thumbnailImageURLString: "https://picsum.photos/300/102",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+let episode2 = Episode(name: "Dark Matter",
+                       season: 1,
+                       episodeNumber: 2,
+                       thumbnailImageURLString: "https://picsum.photos/300/103",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+let episode3 = Episode(name: "Ghosts",
+                       season: 1,
+                       episodeNumber: 3,
+                       thumbnailImageURLString: "https://picsum.photos/300/104",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+let episode4 = Episode(name: "Beginings and Endings",
+                       season: 2,
+                       episodeNumber: 4,
+                       thumbnailImageURLString: "https://picsum.photos/300/105",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+let episode5 = Episode(name: "Beginings and Endings",
+                       season: 2,
+                       episodeNumber: 5,
+                       thumbnailImageURLString: "https://picsum.photos/300/106",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+let episode6 = Episode(name: "Beginings and Endings",
+                       season: 2,
+                       episodeNumber: 6,
+                       thumbnailImageURLString: "https://picsum.photos/300/107",
+                       description: "Six month after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+
+var allExampleEpisodes = [episode1, episode2, episode3, episode4, episode5, episode6]
+
 let exampleMovie1 = Movie(
     id: UUID().uuidString,
     name: "DARK",
