345d15d057f13a704fc88b9be910b587f88eef79
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-08-11 02:55:51 +0900

    update TopRowButtons showing ovelay menus

--- SwiftUI-Netflix/Model/Movie.swift
+++ SwiftUI-Netflix/Model/Movie.swift
@@ -13,7 +13,7 @@ struct Movie: Identifiable {
     var thumbnailURL: URL
     
     var categories: [String]
-    var genre: HomeGenre = .AllGenres
+    var genre: HomeGenre = .AllGenres // default
     
     // MovieDetail View
     var year: Int
